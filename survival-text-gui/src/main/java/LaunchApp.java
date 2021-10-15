b04467b78ef7e64d80815e48840b54db2441cf26
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-10-15 12:46:16 +0200

    Project has been fully initialized

---
+++ survival-text-gui/src/main/java/LaunchApp.java
@@ -0,0 +1,31 @@
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class LaunchApp {
+    public static void main(String[] args) throws InterruptedException {
+
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+
+        Terminal terminal;
+        try {
+            terminal = defaultTerminalFactory.createTerminal();
+            terminal.putCharacter('H');
+            terminal.putCharacter('e');
+            terminal.putCharacter('l');
+            terminal.putCharacter('l');
+            terminal.putCharacter('o');
+
+            terminal.putCharacter('W');
+            terminal.putCharacter('o');
+            terminal.putCharacter('r');
+            terminal.putCharacter('l');
+            terminal.putCharacter('d');
+            terminal.putCharacter('!');
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
